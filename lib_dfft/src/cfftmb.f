76e8f82da6576796fdfd77232c0cd8de92571b4a
Daniel Dagnino <ddagnino@gmail.com>
2017-07-04 17:48:59 +0200

    Add files via upload

---
+++ lib_dfft/src/cfftmb.f
@@ -0,0 +1,60 @@
+C     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+C     *                                                               *
+C     *                  copyright (c) 2011 by UCAR                   *
+C     *                                                               *
+C     *       University Corporation for Atmospheric Research         *
+C     *                                                               *
+C     *                      all rights reserved                      *
+C     *                                                               *
+C     *                     FFTPACK  version 5.1                      *
+C     *                                                               *
+C     *                 A Fortran Package of Fast Fourier             *
+C     *                                                               *
+C     *                Subroutines and Example Programs               *
+C     *                                                               *
+C     *                             by                                *
+C     *                                                               *
+C     *               Paul Swarztrauber and Dick Valent               *
+C     *                                                               *
+C     *                             of                                *
+C     *                                                               *
+C     *         the National Center for Atmospheric Research          *
+C     *                                                               *
+C     *                Boulder, Colorado  (80307)  U.S.A.             *
+C     *                                                               *
+C     *                   which is sponsored by                       *
+C     *                                                               *
+C     *              the National Science Foundation                  *
+C     *                                                               *
+C     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+C
+      SUBROUTINE DCFFTMB (LOT, JUMP, N, INC, C, LENC, WSAVE, LENSAV,
+     1                  WORK, LENWRK, IER)
+      INTEGER  LOT, JUMP, N, INC, LENC, LENSAV, LENWRK, IER
+      COMPLEX       C(LENC)
+      REAL     WSAVE(LENSAV)     ,WORK(LENWRK)
+      LOGICAL DXERCON
+C
+      IER = 0
+C
+      IF (LENC .LT. (LOT-1)*JUMP + INC*(N-1) + 1) THEN
+        IER = 1
+        CALL DXERFFT ('CFFTMB ', 6)
+      ELSEIF (LENSAV .LT. 2*N + INT(LOG(REAL(N))/LOG(2.)) + 4) THEN
+        IER = 2
+        CALL DXERFFT ('CFFTMB ', 8)
+      ELSEIF (LENWRK .LT. 2*LOT*N) THEN
+        IER = 3
+        CALL DXERFFT ('CFFTMB ', 10)
+      ELSEIF (.NOT. DXERCON(INC,JUMP,N,LOT)) THEN
+        IER = 4
+        CALL DXERFFT ('CFFTMB ', -1)
+      ENDIF
+C
+      IF (N .EQ. 1) RETURN
+C
+      IW1 = N+N+1
+      CALL DCMFM1B (LOT,JUMP,N,INC,C,WORK,WSAVE,WSAVE(IW1),
+     1                           WSAVE(IW1+1))
+      RETURN
+      END
